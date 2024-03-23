0be9aaa8373505756af78cef266b04e62d71aaa6
sudhanshu3430 <127764778+sudhanshu3430@users.noreply.github.com>
2024-03-24 00:24:56 +0530

    checkout flow

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -4,13 +4,23 @@ import RestaurantController from "../controllers/RestaurantController";
 const router = express.Router();
 
 router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("restaurantId param must be a string"),
+    RestaurantController.getRestaurant
+);
+
+router.get(
   "/search/:city",
   param("city")
     .isString()
     .trim()
     .notEmpty()
     .withMessage("City param must be a string"),
-    RestaurantController.searchRestaurant
+  RestaurantController.searchRestaurant
 );
 
 export default router;
